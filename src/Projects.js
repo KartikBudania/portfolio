d6736d19e4e2ab84c45c00c747e3d30f5f199c69
Kartik Budania <kartik.budania.02@gmail.com>
2022-12-25 17:22:20 +0530

    project sections

--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,13 @@ const Projects= ()=>{
     return(
         <section id="work">
             <h1>{'<wrapper id= "work">'}</h1>
+            <section className="projects">
+                <div className="leads-tracker"></div>
+                <div className="blackjack-game"></div>
+                <div className="color-picker"></div>
+                <div className="videoController-preloader"></div>
+                <div className="portfolio"></div>
+            </section>
         </section>
     )
 }
