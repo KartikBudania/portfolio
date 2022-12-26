f92a9f7077a62ca48c921a76ae4b55c226e2adfd
Kartik Budania <kartik.budania.02@gmail.com>
2022-12-26 22:28:08 +0530

    react for contactme without the form

---
+++ src/ContactMe.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ContactMe = () =>{
+    return (
+        <section id="contact">
+        <h1>{'<wrapper id= "contact">'}</h1>
+        <div className="details">
+            <div className="detail">
+                <h6>Get in touch 📡</h6>
+                <h3>Have any questions?</h3>
+                <h3>Shoot me an email or find me around the web.</h3>
+                <h2>// Some socials</h2>
+                <p><a href="https://twitter.com/kartik_budania"><i class="fa-brands fa-twitter"></i></a>  <a href="https://github.com/KartikBudania"><i class="fa-brands fa-github"></i></a></p>
+            </div>
+            <div className="detail"></div>
+        </div>
+        <h1>{'</wrapper>'}</h1>
+        </section>
+    )
+};
+
+export default ContactMe;
