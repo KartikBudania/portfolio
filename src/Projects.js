cd269dc796ce5e23466b6f2d974d33ebe5238c9d
Kartik Budania <kartik.budania.02@gmail.com>
2022-12-26 19:12:49 +0530

    little changes in project component

--- src/Projects.js
+++ src/Projects.js
@@ -7,72 +7,54 @@ const Projects= ()=>{
             <section className="projects">
                 <div className="project">
                     <img src="./leadsTracker.png"></img>
-                    <p>// Tech Stack</p>
+                    <h2>// Tech Stack</h2>
                     <ul>
                         <li>REACT</li>
                         <li>JAVASCRIPT</li>
                         <li>HTML</li>
                         <li>CSS</li>
                     </ul>
-                    <p>// See It In Action</p>
-                    <p>This chrome extension using which we cansave important tabs/URLs and can open them whenever needed. Its similar to the bookmark feature in chrome.</p>
-                    <a>Visit the site <i class="fa-solid fa-link"></i></a>
+                    <h2>// See It In Action</h2>
+                    <h3>This chrome extension using which we cansave important tabs/URLs and can open them whenever needed. Its similar to the bookmark feature in chrome.</h3>
                     <a>See the code <i class="fa-brands fa-github"></i></a>
                 </div>
                 <div className="project">
                     <img src="./blackjack.png"></img>
-                    <p>// Tech Stack</p>
+                    <h2>// Tech Stack</h2>
                     <ul>
                         <li>REACT</li>
                         <li>JAVASCRIPT</li>
                         <li>HTML</li>
                         <li>CSS</li>
                     </ul>
-                    <p>// See It In Action</p>
-                    <p>I made this Blackjack game just to spend few hours playing it.</p>
-                    <a>Visit the site <i class="fa-solid fa-link"></i></a>
+                    <h2>// See It In Action</h2>
+                    <h3>I made this Blackjack game just to spend few hours playing it.</h3>
                     <a>See the code <i class="fa-brands fa-github"></i></a>
                 </div>
                 <div className="project">
                     <img src="./colorPicker.png"></img>
-                    <p>// Tech Stack</p>
+                    <h2>// Tech Stack</h2>
                     <ul>
                         <li>REACT</li>
                         <li>JAVASCRIPT</li>
                         <li>HTML</li>
                         <li>CSS</li>
                     </ul>
-                    <p>// See It In Action</p>
-                    <p>I made this webpage whose background color will change randomly as you click on the "Click Me" button, and you can pick any random color you like for your project</p>
-                    <a>Visit the site <i class="fa-solid fa-link"></i></a>
-                    <a>See the code <i class="fa-brands fa-github"></i></a>
-                </div>
-                <div className="project">
-                    <img src="./video.mp4"></img>
-                    <p>// Tech Stack</p>
-                    <ul>
-                        <li>REACT</li>
-                        <li>JAVASCRIPT</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                    </ul>
-                    <p>// See It In Action</p>
-                    <p>In this project I made this video controller and also add a preloader in the starting for the time video is loading</p>
-                    <a>Visit the site <i class="fa-solid fa-link"></i></a>
+                    <h2>// See It In Action</h2>
+                    <h3>I made this webpage whose background color will change randomly as you click on the "Click Me" button, and you can pick any random color you like for your project</h3>
                     <a>See the code <i class="fa-brands fa-github"></i></a>
                 </div>
                 <div className="project">
                     <img src="./portfolio.png"></img>
-                    <p>// Tech Stack</p>
+                    <h2>// Tech Stack</h2>
                     <ul>
                         <li>REACT</li>
                         <li>JAVASCRIPT</li>
                         <li>HTML</li>
                         <li>CSS</li>
                     </ul>
-                    <p>// See It In Action</p>
-                    <p>Not much to say here! This one just gave me a good excuse to spend a weekend practicing with React and CSS</p>
-                    <a>Visit the site <i class="fa-solid fa-link"></i></a>
+                    <h2>// See It In Action</h2>
+                    <h3>Not much to say here! This one just gave me a good excuse to spend a weekend practicing with React and CSS</h3>
                     <a>See the code <i class="fa-brands fa-github"></i></a>
                 </div>
             </section>
