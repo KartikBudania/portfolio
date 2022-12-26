4799a47c31e0b2756a267840642728a00ba5ad45
Kartik Budania <kartik.budania.02@gmail.com>
2022-12-26 22:50:37 +0530

    css for headings

--- src/Projects.js
+++ src/Projects.js
@@ -16,7 +16,7 @@ const Projects= ()=>{
                     </ul>
                     <h2>// See It In Action</h2>
                     <h3>This chrome extension using which we cansave important tabs/URLs and can open them whenever needed. Its similar to the bookmark feature in chrome.</h3>
-                    <a>See the code <i class="fa-brands fa-github"></i></a>
+                    <h3><a>See the code <i class="fa-brands fa-github"></i></a></h3>
                 </div>
                 <div className="project">
                     <img src="./blackjack.png"></img>
@@ -29,7 +29,7 @@ const Projects= ()=>{
                     </ul>
                     <h2>// See It In Action</h2>
                     <h3>I made this Blackjack game just to spend few hours playing it.</h3>
-                    <a>See the code <i class="fa-brands fa-github"></i></a>
+                    <h3><a>See the code <i class="fa-brands fa-github"></i></a></h3>
                 </div>
                 <div className="project">
                     <img src="./colorPicker.png"></img>
@@ -42,7 +42,7 @@ const Projects= ()=>{
                     </ul>
                     <h2>// See It In Action</h2>
                     <h3>I made this webpage whose background color will change randomly as you click on the "Click Me" button, and you can pick any random color you like for your project</h3>
-                    <a>See the code <i class="fa-brands fa-github"></i></a>
+                    <h3><a>See the code <i class="fa-brands fa-github"></i></a></h3>
                 </div>
                 <div className="project">
                     <img src="./portfolio.png"></img>
@@ -55,7 +55,7 @@ const Projects= ()=>{
                     </ul>
                     <h2>// See It In Action</h2>
                     <h3>Not much to say here! This one just gave me a good excuse to spend a weekend practicing with React and CSS</h3>
-                    <a>See the code <i class="fa-brands fa-github"></i></a>
+                    <h3><a>See the code <i class="fa-brands fa-github"></i></a></h3>
                 </div>
             </section>
             <h1>{'</wrapper>'}</h1>
