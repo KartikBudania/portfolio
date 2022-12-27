a06bc705beb5e48d5aaccaebf4960ec5d9b5ac9a
Kartik Budania <kartik.budania.02@gmail.com>
2022-12-27 17:01:14 +0530

    contact form works

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import emailjs from "emailjs-com"
 
 const ContactMe = () =>{
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        emailjs.sendForm(
+        'service_d2lz4gx', 
+        'template_9t2jdcc', 
+        e.target,
+        'xczMJUEQNuJsxTklF'
+        ).then(res=>{
+            console.log(res)
+        }).catch(err=> console.log(err))
+
+        setName('');
+        setEmail('');
+        setMessage('')
+    }
+
     return (
         <section id="contact">
         <h1>{'<wrapper id= "contact">'}</h1>
@@ -13,10 +36,10 @@ const ContactMe = () =>{
                 <p className="socials"><a href="https://twitter.com/kartik_budania"><i className="fa-brands fa-twitter"></i></a>          <a href="https://github.com/KartikBudania"><i className="fa-brands fa-github"></i></a></p>
             </div>
             <div className="detail">
-                <form>
-                    <input type="text" id="name" placeholder="Name" required></input>
-                    <input type="email" id="email" placeholder="Email" required></input>
-                    <textarea id="message" rows="4" placeholder="Message"></textarea>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="name" id="name" placeholder="Name" onChange={event => setName(event.target.value)} value={name} required />
+                    <input type="email" name="user_email" id="email" placeholder="Email" onChange={event => setEmail(event.target.value)} value={email} required />
+                    <textarea name="message" id="message" cols="50" rows="4" placeholder="Message" onChange={event => setMessage(event.target.value)} value={message} required/>
                     <button type="submit">SEND</button>
                 </form>
             </div>
