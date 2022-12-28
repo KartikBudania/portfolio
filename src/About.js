ee02b9999a22410b15b353a7c0fb42076ae8e333
Kartik Budania <kartik.budania.02@gmail.com>
2022-12-28 12:46:02 +0530

    About Section done

--- src/About.js
+++ src/About.js
@@ -5,26 +5,21 @@ const About = () => {
         <section id="about">
         <h1>{'<wrapper id= "about">'}</h1>
         <div className="about">
-        <h3>I'm a Full Stack Developer who's been building industry utilized applications since my second year of college. Since then, I've worked in startups, founded a couple myself, and taken on freelance projects and consulting for clients.</h3>
-        <h2>// Keep reading for more details on my experience ⬇️</h2>
+        {/* <h3>I'm a Full Stack Developer who's been building industry utilized applications since my second year of college. Since then, I've worked in startups, founded a couple myself, and taken on freelance projects and consulting for clients.</h3>
+        <h2>// Keep reading for more details on my experience ⬇️</h2><br/><br/> */}
         <div >
-            <div><h4>0. University Of Victoria</h4>
+            <div><h4>0.<a href="http://www.vishvasschool.com/" target="_blank"> Vishvas Sr. Sec. School, Hisar</a></h4>
             <h5>Student</h5>
-            <h3>2018</h3>
-            <h3>After pursuing a degree in commerce for 2 years, I came across the world of software and technology and immediately shifted in that direction. I transferred in to the Computer Science program with a specialty in Software Systems and studied their the next 3 years.</h3></div>
-            <div><h4>0. University Of Victoria</h4>
+            <h3>2018 - 2020</h3>
+            <h3>I completed high school in physics, chemistry, and mathematics with 80% marks & because my mathematic and logical abilities were quite good, I pursued a career in engineering.</h3></div><br/>
+            <div><h4>1.<a href="http://ipu.ac.in/usict" target="_blank"> USICT, GGSIPU</a></h4>
             <h5>Student</h5>
-            <h3>2018</h3>
-            <h3>After pursuing a degree in commerce for 2 years, I came across the world of software and technology and immediately shifted in that direction. I transferred in to the Computer Science program with a specialty in Software Systems and studied their the next 3 years.</h3></div>
-            <div><h4>0. University Of Victoria</h4>
-            <h5>Student</h5>
-            <h3>2018</h3>
-            <h3>After pursuing a degree in commerce for 2 years, I came across the world of software and technology and immediately shifted in that direction. I transferred in to the Computer Science program with a specialty in Software Systems and studied their the next 3 years.</h3></div>
+            <h3>2020 - 2024</h3>
+            <h3>I'm currently pursuing a Bachelor of Technology in Electronics and Communications Engineering. In my sophomore year, we were taught about Data Structure Algorithms & Operating Systems, which brought me to the software engineering field as I found it fascinating. Therefore, I started learning Web Development and started making projects using Javascript and ReactJS which sharpened my skills.</h3></div>
         </div>
         </div>
         <h1>{'</wrapper>'}</h1>
         </section>
     )
 };
-
 export default About;
