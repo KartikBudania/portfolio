9730ec8340a86ae1c12d130b22060add5a43daf2
Kartik Budania <kartik.budania.02@gmail.com>
2022-12-28 12:51:53 +0530

    about updated

--- src/About.js
+++ src/About.js
@@ -15,7 +15,7 @@ const About = () => {
             <div><h4>1.<a href="http://ipu.ac.in/usict" target="_blank"> USICT, GGSIPU</a></h4>
             <h5>Student</h5>
             <h3>2020 - 2024</h3>
-            <h3>I'm currently pursuing a Bachelor of Technology in Electronics and Communications Engineering. In my sophomore year, we were taught about Data Structure Algorithms & Operating Systems, which brought me to the software engineering field as I found it fascinating. Therefore, I started learning Web Development and started making projects using Javascript and ReactJS which sharpened my skills.</h3></div>
+            <h3>I'm currently in college pursuing a Bachelor of Technology in Electronics and Communications Engineering. In my sophomore year, we were taught about Data Structure Algorithms & Operating Systems, which brought me to the software engineering field as I found it fascinating. Therefore, I started learning Web Development and started making projects using Javascript and ReactJS which sharpened my skills.</h3></div>
         </div>
         </div>
         <h1>{'</wrapper>'}</h1>
