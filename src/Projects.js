e43b030f8187977cca485f05dc09a58330d7720c
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-21 19:40:49 +0530

    added Harmony Project in Projects list

--- src/Projects.js
+++ src/Projects.js
@@ -3,6 +3,7 @@ import leads from './leads.png';
 import blackjack from './blackjack.png';
 import colorPicker from './colorPicker.png';
 import portfolio from './portfolio.png';
+import harmony from './Harmony.png';
 import codeEditor from './Code-Editor.png'
 
 const Projects= ()=>{
@@ -11,7 +12,20 @@ const Projects= ()=>{
             <h1>{'<wrapper id= "work">'}</h1>
             <div className="projects">
                 <div className="project">
-                    <img src={codeEditor} id="firstimg"></img>
+                    <img src={harmony} id="firstimg"></img>
+                    <h2>// Harmony - The Music Player</h2>
+                    <h3>This music player is a web-based application that allows users to stream and listen to their favorite songs and music playlists from anywhere, at any time. It provides a user-friendly interface that is easy to navigate and supports multiple audio formats.</h3>
+                    <h2>// Tech Stack</h2>
+                    <ul className="stacks">
+                        <li className="techstack">REACT</li>
+                        <li className="techstack">HTML</li>
+                        <li className="techstack">CSS</li>
+                    </ul>
+                    <h3><a href="https://kartikbudania.github.io/Harmony/" target="_blank">Visit the site <i className="fa-solid fa-link"></i></a></h3>
+                    <h3><a href="https://github.com/KartikBudania/Harmony" target="_blank">See the code <i className="fa-brands fa-github"></i></a></h3>
+                </div>
+                <div className="project">
+                    <img src={codeEditor}></img>
                     <h2>// Code Editor WebApp</h2>
                     <h3>This online code editor is a web-based application that provides a user-friendly interface for writing and editing code in various programming languages. It allows users to write and test code in real-time, without the need to install any software on their computer.</h3>
                     <h2>// Tech Stack</h2>
