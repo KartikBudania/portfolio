bb4796e55d0d90dac4dd5f260a54b7211ecc8c6c
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-23 22:46:57 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
-import NavBar from './NavBar';
-import Hero from './Hero';
-import Projects from './Projects';
-import About from './About';
-import Tools from './Tools';
-import ContactMe from './ContactMe';
-import Footer from './Footer';
-
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+import Projects from "./Projects";
+import About from "./About";
+import Tools from "./Tools";
+import ContactMe from "./ContactMe";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <Hero/>
-      <Projects/>
-      <About/>
-      <Tools/>
-      <ContactMe/>
-      <Footer/>
+      <NavBar />
+      <Hero />
+      <Projects />
+      <About />
+      <Tools />
+      <ContactMe />
+      <Footer />
     </>
   );
 }
