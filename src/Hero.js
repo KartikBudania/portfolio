4359eb564613c3b5cd96fd1d5930baa9a1faec49
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-25 22:12:16 +0530

    Hero.js

--- src/Hero.js
+++ src/Hero.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-const Hero= ()=>{
-    return(
-        <section className="hero">
-            <div className="hero-heading">Hello, I'm Kartik 👋</div>
-            <div className="hero-banner">
-                I'm a Front-End Developer,<br></br>
-                die hard DragonBall Z fan, &<br></br>
-                a Basketball player 🏀.
-            </div>
-            <div className="scroll-down"></div>
-        </section>
-    )
-}
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-heading">Hello, I'm Kartik 👋</div>
+      <div className="hero-banner">
+        I'm a Front-End Developer,<br></br>
+        die hard DragonBall Z fan, &<br></br>a Basketball player 🏀.
+      </div>
+      <div className="scroll-down"></div>
+    </section>
+  );
+};
 
 export default Hero;
